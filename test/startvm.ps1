12f111190ce1cbe2359d5dc810269d3be5378f90
Chaitanya Chougule <67632729+chaitanya867@users.noreply.github.com>
2022-05-05 13:25:23 +0530

    Update startvm.ps1

--- test/startvm.ps1
+++ test/startvm.ps1
@@ -1,5 +1,5 @@
 Connect-AzAccount -Identity
 $ResourceGroupName = 'test-RG' 
 $AzureVMName = 'test-vm'   
-"Starting Azure VM..."
+"Starting Azure VM............"
 Start-AzureRmVM -Name $AzureVMName -ResourceGroupName $ResourceGroupName
